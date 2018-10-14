57bac60709edce6b9b9401752a3a40460e19c261
Sam <sam@meeum.com>
2018-10-14 12:08:01 +1100

    nav js

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,1 +1,24 @@
 cssVars();
+
+// open navigation
+
+$('.nav-prompt').click(function () {
+
+    if ($('.nav-inner').attr('aria-expanded') == 'false') {
+
+        $('.nav-inner').attr('aria-expanded', 'true');
+
+    } else {
+        $('.nav-inner').attr('aria-expanded', 'false');
+    }
+
+    // change the text
+    if ($('.nav-prompt').text() == 'Open Navigation') {
+        
+        $('.nav-prompt').text('Close Navigation');
+
+    } else {
+        $('.nav-prompt').text('Open Navigation');
+    }
+
+})
